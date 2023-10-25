9b3b23618dbf36495b051e375224ba5cde8ecb07
PKH0208 <137423200+PKH0208@users.noreply.github.com>
2023-10-25 18:52:55 +0900

    Update openc_v.py
    Signed-off-by: PKH0208 <137423200+PKH0208@users.noreply.github.com>

--- openc_v.py
+++ openc_v.py
@@ -12,13 +12,26 @@ from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
 import pathlib
+from sklearn.preprocessing import LabelBinarizer
+from tensorflow.keras.utils import to_categorical
 
-# 데이터 디렉토리 설정
+# 이미지를 저장할 기본 디렉토리 설정
 data_dir = pathlib.Path("C:/Users/KITCOOP/kicpython/hansik/kfood_new/learning_rgb1/image/")
-data_dir_test = pathlib.Path("C:/Users/KITCOOP/kicpython/hansik/kfood_new/test_rgb1/")
+data_dir_test = pathlib.Path("C:/Users/KITCOOP/kicpython/hansik/kfood_new/test_rgb1/image/")
 
-image_count = len(list(data_dir.glob('*/*.jpg')))
-image_test_count = len(list(data_dir_test.glob('*/*.jpg')))
+# 이미지 확장자 목록
+valid_image_extensions = (".jpg", ".jpeg", ".png", ".gif")
+
+# 이미지 파일 목록을 저장할 리스트 초기화
+image_paths = []
+
+# 데이터 디렉토리에서 이미지 파일 목록을 가져옴
+for ext in valid_image_extensions:
+    image_paths.extend(list(data_dir.glob(f"*/*{ext}")))
+    image_paths.extend(list(data_dir_test.glob(f"*/*{ext}")))
+
+# 이미지 개수 계산
+image_count = len(image_paths)
 
 # 이미지 크기 및 배치 크기 설정
 batch_size = 512
@@ -30,14 +43,16 @@ train_ds = tf.keras.utils.image_dataset_from_directory(
     data_dir,
     validation_split=None,
     image_size=(img_height, img_width),
-    batch_size=batch_size
+    batch_size=batch_size,
+    label_mode="int"  # 레이블을 정수로 설정
 )
 
 val_ds = tf.keras.utils.image_dataset_from_directory(
     data_dir_test,
     validation_split=None,
     image_size=(img_height, img_width),
-    batch_size=batch_size
+    batch_size=batch_size,
+    label_mode="int"  # 레이블을 정수로 설정
 )
 
 # 이미지 클래스 이름 확인
@@ -59,8 +74,24 @@ data_augmentation = keras.Sequential([
     layers.RandomZoom(0.1)
 ])
 
+# 레이블 데이터 추출
+train_labels = []
+val_labels = []
+
+for image_path in image_paths:
+    # 파일 경로에서 레이블 추출
+    label = pathlib.Path(image_path).parent.name
+    
+    # train 또는 validation에 따라 레이블 데이터를 나눔
+    if "learning_rgb1" in str(image_path):
+        train_labels.append(label)
+    else:
+        val_labels.append(label)
+
+train_labels = np.array(train_labels, dtype=np.object)
+val_labels = np.array(val_labels, dtype=np.object)
+
 num_classes = 300
-# CNN 모델 정의
 model = Sequential([
     data_augmentation,
     layers.Rescaling(1./255),
@@ -73,12 +104,12 @@ model = Sequential([
     layers.Dropout(0.2),
     layers.Flatten(),
     layers.Dense(128, activation='relu'),
-    layers.Dense(num_classes, name="outputs")
-])
+    layers.Dense(num_classes, activation='softmax', name="outputs")
+    ])
 
 # 모델 컴파일 및 학습
 model.compile(optimizer='adam',
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              loss='sparse_categorical_crossentropy',  # 다중 클래스 분류 문제의 손실 함수
               metrics=['accuracy'])
 
 epochs = 20
@@ -88,21 +119,20 @@ history = model.fit(train_ds, validation_data=val_ds, epochs=epochs)
 model.evaluate(val_ds)
 
 # 이미지 테스트
-test_path = "C:/Users/KITCOOP/kicpython/hansik/kfood_test/test_rgb1/"
-Image.open(test_path)
+test_path = "C:/Users/KITCOOP/kicpython/hansik/kfood_test/test_rgb1/image/"
 
-# 이미지 자동 사이징 및 예측
-img = tf.keras.utils.load_img(
-    test_path, target_size=(img_height, img_width)
-)
+# 이미지 파일 읽어오기
+image = tf.io.read_file(test_path)
+image = tf.image.decode_image(image)  # 이미지 파일 형식 자동 감지
 
-img_array = tf.keras.utils.img_to_array(img)
-img_array = tf.expand_dims(img_array, 0)  # 배치 생성
+# 이미지 자동 사이즈 조정 및 예측
+image_array = tf.expand_dims(image, 0)  # 배치 생성
 
-predictions = model.predict(img_array)
-score = tf.nn.softmax(predictions[0])
+predictions = model.predict(image_array)
+scores = tf.nn.softmax(predictions[0])
 
-print("이미지는 {} with a {:.2f} percent 확신합니다.".format(class_names[np.argmax(score)], 100 * np.max(score)))
+for i in range(len(class_names)):
+    print("클래스 '{}': {:.2f}% 확률로 속함".format(class_names[i], 100 * scores[i]))
 
 # 예측이 어려운 데이터 처리 및 기록 및 로깅
 def process_uncertain_prediction(prediction, threshold=0.6):
@@ -118,5 +148,5 @@ def handle_errors_and_monitoring(data, model):
             processed_result = process_uncertain_prediction(prediction)
             print(f"예측: {processed_result}, 실제 레이블: {labels[i]}")
 
-# 오류 처리 및 모델 모니터링 실행
+# 이미지 테스트와 오류 처리 및 모델 모니터링 실행
 handle_errors_and_monitoring(val_ds, model)
