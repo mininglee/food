09db05a69f58606280f8fbc486f4ae341e9cadb2
mininglee <129207204+mininglee@users.noreply.github.com>
2023-10-27 02:27:41 +0900

    Update Food_feedback.py
    Signed-off-by: mininglee <129207204+mininglee@users.noreply.github.com>

--- kfood_new(새로운 폴더)/function/Food_feedback.py
+++ kfood_new(새로운 폴더)/function/Food_feedback.py
@@ -19,8 +19,7 @@ class FoodLog:
             print(f"칼로리: {entry['calories']} kcal")
             print(f"단백질: {entry['protein']} g")
             print(f"탄수화물: {entry['carbs']} g")
-            print(f"지방: {entry['fat']} g")
-            print(f"나트륨: {entry['sodium']} mg")
+            print(f"식이섬유: {entry['fibre']} g")
             print("-" * 30)
 
     def calculate_excess(self, daily_intake):
@@ -52,6 +51,24 @@ def provide_info_and_recommendations(excess):
         print("- 단백질 섭취를 조절")
         print("- 식사에서 식물성 단백질을 추가")
         print("대체 음식 추천: 채소 기반 단백질 식사")
+        
+    if excess['carbohydrates'] > 0:
+        print("과다한 탄수화물 섭취로 인해 다음과 같은 증상이 나타날 수 있습니다:")
+        print("- 체중 증가")
+        print("- 혈당 상슨")
+        print("- 췌장 문제")
+        print("조치를 취하기 위해 다음을 고려하세요:")
+        print("- 탄수화물 섭취를 제한")
+        print("- 고섬유 식품을 섭취")
+        print("대체 음식 추천: 꾸준한 탄수화물 섭취를 유지하면서 고섬유 식품 섭취")
+    
+    if excess['fiber'] < recommended_fiber:
+        print("섭취한 식이섬유가 권장량보다 부족합니다.")
+        print("식이섬유는 소화를 돕고 건강한 소화계를 유지하는 데 중요합니다.")
+        print("조치를 취하기 위해 다음을 고려하세요:")
+        print("- 과일, 채소, 곡물 및 견과류를 더 많이 섭취")
+        print("- 식이섬유 풍부한 식품을 식단에 추가")
+        print("대체 음식 추천: 브로콜리, 시금치, 아보카도 및 곡물을 더 많이 섭취")
 
     # 다른 영양소에 대한 증상 정보 및 대체 음식 정보 추가
 
@@ -70,8 +87,7 @@ def main():
             calories = float(input("칼로리: "))
             protein = float(input("단백질 (g): "))
             carbs = float(input("탄수화물 (g): "))
-            fat = float(input("지방 (g): "))
-            sodium = float(input("나트륨 (mg): "))
+            fibre = float(input("식이섬유 (g): "))
             food_logger.record_food(food_name, calories, protein, carbs, fat, sodium)
             print("음식이 기록되었습니다.")
         elif choice == '2':
@@ -81,8 +97,7 @@ def main():
                 "calories": float(input("하루 권장 칼로리 (kcal): ")),
                 "protein": float(input("하루 권장 단백질 (g): ")),
                 "carbs": float(input("하루 권장 탄수화물 (g): ")),
-                "fat": float(input("하루 권장 지방 (g): ")),
-                "sodium": float(input("하루 권장 나트륨 (mg): "))
+                "fibre": float(input("하루 권장 식이섬유 (g): "))
             }
             excess = food_logger.calculate_excess(daily_intake)
             provide_info_and_recommendations(excess)
