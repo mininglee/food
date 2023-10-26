9fff875d57aadc0286be7b536d50048da6fb25eb
mininglee <129207204+mininglee@users.noreply.github.com>
2023-10-27 02:28:08 +0900

    Update Food_recommend.py
    Signed-off-by: mininglee <129207204+mininglee@users.noreply.github.com>

--- kfood_new(새로운 폴더)/function/Food_recommend.py
+++ kfood_new(새로운 폴더)/function/Food_recommend.py
@@ -1,32 +1,16 @@
-import requests
-from bs4 import BeautifulSoup
+import pandas as pd
 
 class FoodRecommendation:
     def __init__(self):
         self.food_database = self.update_food_database()
     
     def update_food_database(self):
-        # 웹 페이지에서 음식 정보 크롤링
-        url = "https://example.com/food_data"  # 원하는 웹 페이지의 URL을 입력
-        response = requests.get(url)
-        soup = BeautifulSoup(response.content, "html.parser")
+        # 엑셀 파일에서 음식 정보 불러오기
+        excel_file_path = "C:/Users/KITCOOP/kicpython/hansik/kfood_new/xlsx/xlsx_b.xlsx"  # 엑셀 파일 경로
+        df = pd.read_excel(excel_file_path)
 
-        # 웹 페이지에서 음식 정보를 추출하여 업데이트
-        food_database = []
-        for item in soup.find_all("div", class_="food-item"):
-            food_name = item.find("span", class_="food-name").text
-            calories = float(item.find("span", class_="calories").text)
-            protein = float(item.find("span", class_="protein").text)
-            carbs = float(item.find("span", class_="carbs").text)
-            fat = float(item.find("span", class_="fat").text)
-            food_entry = {
-                "food_name": food_name,
-                "calories": calories,
-                "protein": protein,
-                "carbs": carbs,
-                "fat": fat
-            }
-            food_database.append(food_entry)
+        # 엑셀 파일에서 음식 정보를 데이터베이스로 변환
+        food_database = df.to_dict(orient='records')
         
         return food_database
 
@@ -51,15 +35,15 @@ def main():
 
         if choice == '1':
             shortage = {
-                "calories": float(input("부족한 칼로리 (kcal): ")),
-                "protein": float(input("부족한 단백질 (g): ")),
-                "carbs": float(input("부족한 탄수화물 (g): ")),
-                "fat": float(input("부족한 지방 (g): "))
+                "칼로리": float(input("부족한 칼로리 (kcal): ")),
+                "단백질": float(input("부족한 단백질 (g): ")),
+                "탄수화물": float(input("부족한 탄수화물 (g): ")),
+                "식이섬유": float(input("식이섬유 (g): "))
             }
             recommended_foods = food_recommendation.recommend_foods(shortage)
             print("부족한 영양소를 보충할 수 있는 음식 추천:")
             for food in recommended_foods:
-                print(f"음식: {food['food_name']}")
+                print(f"음식: {food['음식 이름']}")
                 print(f"{list(shortage.keys())[list(shortage.values()).index(min(shortage.values()))]} 보충용으로 추천됩니다.")
                 print("-" * 30)
         elif choice == '2':
