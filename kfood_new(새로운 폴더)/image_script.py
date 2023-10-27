219cecb736d90390349442a9f1258ae73fbd60cb
PKH0208 <137423200+PKH0208@users.noreply.github.com>
2023-10-27 19:56:30 +0900

    Update image_script.py
    Signed-off-by: PKH0208 <137423200+PKH0208@users.noreply.github.com>

--- kfood_new(새로운 폴더)/image_script.py
+++ kfood_new(새로운 폴더)/image_script.py
@@ -1,18 +1,20 @@
 import os
 import tensorflow as tf
-from tensorflow import keras
+from tensorflow.keras.models import load_model, predict_image
 import numpy as np
 
 # 모델을 불러오는 코드
 def load_model(model_path):
-    model = keras.models.load_model(model_path)
+    model = load_model(model_path)
     return model
 
 # 이미지를 예측하는 함수
-def predict_image(model, img_path, img_height, img_width):
+def predict_image(model, img_path, img_height, img_width, class_labels):
     img = tf.keras.preprocessing.image.load_img(img_path, target_size=(img_height, img_width))
     img_array = tf.keras.preprocessing.image.img_to_array(img)
     img_array = tf.expand_dims(img_array, 0)
     predictions = model.predict(img_array)
     predicted_class = np.argmax(predictions)
-    return predicted_class
+    class_name = class_labels[predicted_class]
+    probability = np.max(predictions)
+    return predicted_class, class_name, probability
